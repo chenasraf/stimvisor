4721dbc02dba70b3e238cccd24fb5053f0f4e80f
Chen Asraf <casraf@pm.me>
2024-10-23 01:49:39 +0300

    perf: screenshots modal rendering

--- frontend/src/components/ScreenshotImg/ScreenshotImg.tsx
+++ frontend/src/components/ScreenshotImg/ScreenshotImg.tsx
@@ -5,9 +5,17 @@ import { cn } from '@/common/utils'
 export function ScreenshotImg({
   className,
   screenshot,
+  load = true,
   ...rest
-}: Omit<HtmlProps<'img'>, 'src'> & { screenshot: screenshots.ScreenshotEntry }) {
-  return (
-    <img className={cn('rounded-md', rest.onClick && 'cursor-pointer', className)} src={screenshot.base64} alt={screenshot.name} {...rest} />
+}: Omit<HtmlProps<'img'>, 'src'> & { screenshot: screenshots.ScreenshotEntry; load?: boolean }) {
+  return load ? (
+    <img
+      className={cn('rounded-md', rest.onClick && 'cursor-pointer', className)}
+      src={screenshot.base64}
+      alt={screenshot.name}
+      {...rest}
+    />
+  ) : (
+    <div className={cn('rounded-md', rest.onClick && 'cursor-pointer', className)} />
   )
 }
--- frontend/src/components/ScreenshotsCarouselModal/ScreenshotsCarouselModal.tsx
+++ frontend/src/components/ScreenshotsCarouselModal/ScreenshotsCarouselModal.tsx
@@ -8,37 +8,69 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
+  useCarousel,
 } from '../ui/carousel'
 import { ScreenshotImg } from '../ScreenshotImg/ScreenshotImg'
+import { useCallback, useEffect, useState } from 'react'
 
-export function ScreenshotsCarouselModal({
-  className,
-  screenshots,
-  activeIndex,
-  ...rest
-}: HtmlProps<'div'> & {
-  screenshots: screenshots.ScreenshotEntry[]
-  activeIndex?: number | null
-}) {
+export function ScreenshotsCarouselModal(
+  props: HtmlProps<'div'> & {
+    screenshots: screenshots.ScreenshotEntry[]
+    activeIndex?: number | null
+  },
+) {
+  const { className, activeIndex, ...rest } = props
   return (
     <div className={cn('', className)} {...rest}>
       <DialogContent className="max-w-[calc(100%_-_128px)] max-h-[calc(100%_-_64px)]">
         <DialogTitle>Screenshots</DialogTitle>
-        <Carousel opts={{ startIndex: activeIndex ?? undefined }}>
-          <CarouselContent className="max-h-full">
-            {screenshots.map((scr) => (
-              <CarouselItem key={scr.path} className="flex items-center justify-center">
-                <ScreenshotImg
-                  screenshot={scr}
-                  className="max-h-[calc(100vh_-_160px)] object-cover mx-auto"
-                />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
+        <Carousel opts={{ startIndex: activeIndex ?? undefined, loop: true }}>
+          <CarouselInner {...props} />
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
       </DialogContent>
     </div>
+  )
+}
+
+function CarouselInner({
+  activeIndex,
+  screenshots,
+}: React.ComponentProps<typeof ScreenshotsCarouselModal>) {
+  const carousel = useCarousel()
+  const [inView, setInView] = useState(() => carousel.api?.slidesInView() ?? [])
+  const isInView = useCallback(
+    (idx: number) =>
+      [-2, -1, 0, 1, 2].map((x) => idx + x).some((x) => activeIndex === x || inView.includes(x)),
+    [activeIndex, inView],
+  )
+
+  useEffect(() => {
+    if (!carousel.api) {
+      return
+    }
+    const { api } = carousel
+    const cb = () => {
+      setInView(api.slidesInView() ?? [])
+    }
+    api.on('slidesInView', cb)
+    return () => {
+      api.off('slidesInView', cb)
+    }
+  }, [carousel])
+
+  return (
+    <CarouselContent className="max-h-full">
+      {screenshots.map((scr, i) => (
+        <CarouselItem key={scr.path} className="flex items-center justify-center">
+          <ScreenshotImg
+            screenshot={scr}
+            load={isInView(i)}
+            className="max-h-[calc(100vh_-_160px)] object-cover mx-auto"
+          />
+        </CarouselItem>
+      ))}
+    </CarouselContent>
   )
 }
